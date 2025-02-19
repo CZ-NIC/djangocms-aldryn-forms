5a956e8abc590c388ceddc66740b54b0d6cb84d3
Zdeněk Böhm <zdenek.bohm@seznam.cz>
2025-02-19 12:06:28 +0100

    Setup build compile npm.

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
+from distutils.command.build import build
+
 from setuptools import setup
+from setuptools_npm import npm_not_skipped
+
+
+class custom_build(build):
+    sub_commands = [
+        ("compile_catalog", lambda x: True),
+        ("npm_install", npm_not_skipped),
+        ("npm_run", npm_not_skipped),
+    ] + build.sub_commands
 
 
 def main():
-    setup(test_suite='tests.settings.run')
+    setup(test_suite='tests.settings.run', cmdclass={"build": custom_build})
 
 
 if __name__ == "__main__":

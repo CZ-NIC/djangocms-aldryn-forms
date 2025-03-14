77b720649d8fffc56d2fac926c90e4499a0e956e
Zdeněk Böhm <zdenek.bohm@seznam.cz>
2025-03-14 16:54:05 +0100

    Devel test_management_commands.

--- tests/test_management_commands.py
+++ tests/test_management_commands.py
@@ -1,10 +1,13 @@
 import json
 from datetime import datetime, timezone
 
+from django.core import mail
 from django.core.management import call_command
 from django.test import TestCase, override_settings
 
+import responses
 from freezegun import freeze_time
+from testfixtures import LogCapture
 
 from aldryn_forms.models import FormSubmission, SubmittedToBeSent, Webook
 
@@ -31,16 +34,26 @@ class RemoveExpiredPostIdentsTest(TestCase):
         self.assertQuerySetEqual(FormSubmission.objects.values_list('post_ident'), [(None,)], transform=None)
 
 
+@override_settings(ALDRYN_FORMS_MULTIPLE_SUBMISSION_DURATION=30)
 class SendEmailsTest(TestCase):
 
     data = [
         {"label": "Test", "name": "test", "value": 1},
     ]
 
+    def setUp(self):
+        self.url = "https://host.foo/webhook/"
+        self.log_handler = LogCapture()
+        self.addCleanup(self.log_handler.uninstall)
+
     def test_not_yet_removed(self):
-        Webook.objects.create(name="Test", url="http:/test.foo/webhook/")
         with freeze_time(datetime(2025, 3, 14, 9, 0, tzinfo=timezone.utc)):
-            SubmittedToBeSent.objects.create(name="Test", data=json.dumps(self.data), post_ident="1234567890")
+            tosent = SubmittedToBeSent.objects.create(name="Test", data=json.dumps(self.data), post_ident="1234567890")
+        webhook = Webook.objects.create(name="Test", url=self.url)
+        tosent.webhooks.add(webhook)
         with freeze_time(datetime(2025, 3, 14, 9, 30, tzinfo=timezone.utc)):
-            call_command("aldryn_forms_send_emails")
-        self.assertQuerySetEqual(FormSubmission.objects.values_list('post_ident'), [("1234567890",)], transform=None)
+            with responses.RequestsMock():
+                call_command("aldryn_forms_send_emails")
+        self.assertQuerySetEqual(SubmittedToBeSent.objects.values_list('post_ident'), [("1234567890",)], transform=None)
+        self.assertEqual(len(mail.outbox), 0)
+        self.log_handler.check()

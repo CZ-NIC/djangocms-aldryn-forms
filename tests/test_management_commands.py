0222649623ed5d141e20a535d61b267f3acbe476
Zdeněk Böhm <zdenek.bohm@nic.cz>
2025-03-17 14:21:29 +0100

    More tests in test_management_commands.

--- tests/test_management_commands.py
+++ tests/test_management_commands.py
@@ -33,6 +33,14 @@ class RemoveExpiredPostIdentsTest(TestCase):
             call_command("aldryn_forms_remove_expired_post_idents")
         self.assertQuerySetEqual(FormSubmission.objects.values_list('post_ident'), [(None,)], transform=None)
 
+    @override_settings(ALDRYN_FORMS_MULTIPLE_SUBMISSION_DURATION=0)
+    def test_disabled(self):
+        with freeze_time(datetime(2025, 3, 14, 8, 59, 59, tzinfo=timezone.utc)):
+            FormSubmission.objects.create(name="Test", data=json.dumps(self.data), post_ident="1234567890")
+        with freeze_time(datetime(2025, 3, 14, 9, 30, tzinfo=timezone.utc)):
+            call_command("aldryn_forms_remove_expired_post_idents")
+        self.assertQuerySetEqual(FormSubmission.objects.values_list('post_ident'), [("1234567890",)], transform=None)
+
 
 @override_settings(ALDRYN_FORMS_MULTIPLE_SUBMISSION_DURATION=30)
 class SendEmailsTest(TestCase):
@@ -68,6 +76,28 @@ class SendEmailsTest(TestCase):
 
     def test_post_removed(self):
         with freeze_time(datetime(2025, 3, 14, 8, 59, 59, tzinfo=timezone.utc)):
+            tosent = SubmittedToBeSent.objects.create(
+                name="Test",
+                data=json.dumps(self.data),
+                recipients=json.dumps(self.recipients),
+                post_ident="1234567890"
+            )
+        webhook = Webook.objects.create(name="Test", url=self.url)
+        tosent.webhooks.add(webhook)
+        with freeze_time(datetime(2025, 3, 14, 9, 30, tzinfo=timezone.utc)):
+            with responses.RequestsMock() as rsps:
+                rsps.add(responses.POST, self.url, body=json.dumps([{"status": "OK"}]))
+                call_command("aldryn_forms_send_emails")
+        self.assertEqual(SubmittedToBeSent.objects.count(), 0)
+        self.assertEqual(len(mail.outbox), 1)
+        msg = mail.outbox[0].message()
+        self.assertEqual(msg.get("to"), "dave@foo.foo")
+        self.assertEqual(msg.get("subject"), "[Form submission] Test")
+        self.log_handler.check()
+
+    @override_settings(ALDRYN_FORMS_MULTIPLE_SUBMISSION_DURATION=0)
+    def test_disabled(self):
+        with freeze_time(datetime(2025, 3, 14, 9, 0, tzinfo=timezone.utc)):
             tosent = SubmittedToBeSent.objects.create(
                 name="Test",
                 data=json.dumps(self.data),

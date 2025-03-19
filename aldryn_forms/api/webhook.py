8d9b4557833aada2c7045b8b65ef875b9fdef459
Zdeněk Böhm <zdenek.bohm@nic.cz>
2025-03-19 15:11:49 +0100

    Transform webhook data.

--- aldryn_forms/api/webhook.py
+++ aldryn_forms/api/webhook.py
@@ -1,9 +1,14 @@
 import json
 import logging
-from typing import TYPE_CHECKING
+import re
+from typing import TYPE_CHECKING, Dict, List, Optional, Union
 
+import jq
 import requests
 from requests.exceptions import RequestException
+
+
+dataType = Dict[str, str]
 
 
 if TYPE_CHECKING:  # pragma: no cover
@@ -15,9 +20,12 @@ from django.db.models import ManyToManyField
 logger = logging.getLogger(__name__)
 
 
-def send_to_webook(url: str, data: str) -> requests.Response:
+def send_to_webook(url: str, method: str, data: dataType) -> requests.Response:
     """Send data to URL as POST."""
-    response = requests.post(url, data, headers={"Content-Type": "application/json"})
+    if method == "JSON":
+        response = requests.post(url, json.dumps(data), headers={"Content-Type": "application/json"})
+    else:
+        response = requests.post(url, data)
     response.raise_for_status()
     return response
 
@@ -26,9 +34,44 @@ def trigger_webhooks(webhooks: ManyToManyField, instance: "FormSubmissionBase", 
     """Trigger webhooks and send them the instance data."""
     from aldryn_forms.api.serializers import FormSubmissionSerializer
     serializer = FormSubmissionSerializer(instance, context={"hostname": hostname})
-    payload = json.dumps(serializer.data)
+
     for hook in webhooks.all():
+        data = transform_data(hook.transform, serializer.data)
         try:
-            send_to_webook(hook.url, payload)
+            send_to_webook(hook.url, hook.method, data)
         except RequestException as err:
             logger.error(f"{hook.url} {err}")
+
+
+def transform_data(transform: Optional[List[dataType]], data: dataType) -> dataType:
+    """Transform data according to rules."""
+    if transform is None:
+        return data
+    out: dataType = {}
+    for rule in transform:
+        if "value" in rule:
+            out[rule["dest"]] = rule["value"]
+        else:
+            chunks = []
+            src = [rule["src"]] if isinstance(rule["src"], str) else rule["src"]
+            for query in src:
+                input = jq.compile(query).input(data)
+                value = getattr(input, rule.get("fetcher", "first"))()
+                chunks.append(str(value))
+            if chunks:
+                value = " ".join(chunks)
+                if "match" in rule:
+                    value = process_match(rule["match"], value)
+                if value:
+                    out[rule["dest"]] = value
+    return out
+
+
+def process_match(pattern: Union[str, List], value: str) -> str:
+    """Process match."""
+    if isinstance(pattern, str):
+        match = re.match(pattern, value)
+    else:
+        pattern, flags = pattern
+        match = re.match(pattern, value, getattr(re, flags))
+    return match.group(1) if match else value

bf016695582e97b53c3c7699d552a32bf243f551
Zdeněk Böhm <zdenek.bohm@seznam.cz>
2025-03-20 12:08:07 +0100

    Add action export_webhooks.

--- aldryn_forms/admin/options.py
+++ aldryn_forms/admin/options.py
@@ -1,9 +1,15 @@
-from django.contrib import admin
+from urllib.parse import urlencode
+
+from django import forms
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
-from django.urls import path
+from django.urls import path, reverse
+from django.utils.translation import gettext_lazy as _
 
 from tablib import Dataset
 
+from ..models import Webhook
 from .base import BaseFormSubmissionAdmin
 from .forms import WebhookAdminForm
 from .views import FormExportWizardView
@@ -21,9 +27,14 @@ def get_supported_format():
     return 'csv'
 
 
+class SelectWebhookForm(forms.Form):
+    webhook = forms.ChoiceField(choices=Webhook.objects.values_list("pk", "name").order_by("name"))
+
+
 class FormSubmissionAdmin(BaseFormSubmissionAdmin):
     readonly_fields = BaseFormSubmissionAdmin.readonly_fields + ['form_url']
     search_fields = ["data"]
+    actions = ["export_webhook"]
 
     def get_form_export_view(self):
         return FormExportWizardView.as_view(admin=self, file_type=get_supported_format())
@@ -36,14 +47,32 @@ class FormSubmissionAdmin(BaseFormSubmissionAdmin):
     def get_urls(self):
         urls = super().get_urls()
         return [
-            path("webhooks-export/", self.admin_site.admin_view(self.webhook_export), name="webhook_export")
+            path("webhook-export/", self.admin_site.admin_view(self.webhook_export), name="webhook_export")
         ] + urls
 
     def webhook_export(self, request):
-        # http://localhost:8222/en/admin/aldryn_forms/formsubmission/webhooks-export/
-        form = None
-        context = dict(self.admin_site.each_context(request), form=form)
+        if request.method == "POST":
+            form = SelectWebhookForm(request.POST)
+            if form.is_valid():
+                webhook_id = form.cleaned_data["webhook"]
+                messages.success(request, _("An success message that will be displayed."))
+                return HttpResponseRedirect(reverse("admin:aldryn_forms_formsubmission_changelist"))
+        else:
+            form = SelectWebhookForm()
+        data = {
+            "ids": request.GET.get("ids"),
+            "form": form,
+        }
+        context = dict(self.admin_site.each_context(request), **data)
         return TemplateResponse(request, "admin/aldryn_forms/formsubmission/webhook_form.html", context)
+
+    @admin.action(description='Export formatted by webhook', permissions=['change'])
+    def export_webhook(self, request, queryset):
+        selected = queryset.values_list("pk", flat=True)
+        params = urlencode({"ids": ".".join([str(pk) for pk in selected])})
+        path = reverse("admin:webhook_export")
+        return HttpResponseRedirect(f"{path}?{params}")
+    export_webhook.short_description = 'Export formatted by webhook'
 
 
 class WebhookAdmin(admin.ModelAdmin):

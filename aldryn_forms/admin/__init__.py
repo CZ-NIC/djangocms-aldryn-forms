1c2e0cfa20d88e94a5c4c8f2ea85312e18bc9edf
Zdeněk Böhm <zdenek.bohm@seznam.cz>
2025-03-23 08:16:31 +0100

    Remove admin/options.

--- aldryn_forms/admin/__init__.py
+++ aldryn_forms/admin/__init__.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
 
+from tablib import Dataset
+
 from ..models import FormSubmission, Webhook
-from .options import FormSubmissionAdmin, WebhookAdmin
+from .base import BaseFormSubmissionAdmin
+from .forms import WebhookAdminForm
+from .views import FormExportWizardView
+
+
+def get_supported_format():
+    """Get supported format from types xlsx, xls, tsv or cvs."""
+    dataset = Dataset()
+    for ext in ('xlsx', 'xls'):
+        try:
+            getattr(dataset, ext)
+            return ext
+        except (ImportError, AttributeError):
+            pass
+    return 'csv'
+
+
+class FormSubmissionAdmin(BaseFormSubmissionAdmin):
+    readonly_fields = BaseFormSubmissionAdmin.readonly_fields + ['form_url']
+
+    def get_form_export_view(self):
+        return FormExportWizardView.as_view(admin=self, file_type=get_supported_format())
+
+
+class WebhookAdmin(admin.ModelAdmin):
+    form = WebhookAdminForm
 
 
 admin.site.register(FormSubmission, FormSubmissionAdmin)
--- aldryn_forms/admin/base.py
+++ aldryn_forms/admin/base.py
@@ -1,15 +1,28 @@
+import re
 from email.utils import formataddr
+from typing import Callable
+from urllib.parse import urlencode
 
+from django import forms
 from django.conf import settings
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.sites.models import Site
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template.loader import render_to_string
+from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.utils.encoding import force_str
 from django.utils.module_loading import import_string
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 
 from import_export.fields import Field
 from import_export.resources import Resource
+
+from ..api.webhook import collect_submissions_data, send_submissions_data
+from ..models import FormSubmission, Webhook
+from .utils import PrettyJsonEncoder
 
 
 str_dunder_method = '__str__'
@@ -39,6 +52,8 @@ class BaseFormSubmissionAdmin(admin.ModelAdmin):
     date_hierarchy = 'sent_at'
     list_display = [str_dunder_method, 'sent_at', 'language', 'display_data']
     list_filter = ['name', 'language']
+    search_fields = ["data"]
+    actions = ["export_webhook", "send_webhook"]
     readonly_fields = [
         'name',
         'get_data_for_display',
@@ -71,6 +86,15 @@ class BaseFormSubmissionAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
+    def get_search_results(self, request, queryset, search_term):
+        queryset, may_have_duplicates = super().get_search_results(request, queryset, search_term)
+        try:
+            re.match(search_term, "")
+            queryset |= self.model.objects.filter(data__regex=search_term)
+        except Exception as err:
+            messages.error(request, err)
+        return queryset, may_have_duplicates
+
     def get_data_for_display(self, obj):
         data = obj.get_form_data()
         html = render_to_string(
@@ -102,6 +126,8 @@ class BaseFormSubmissionAdmin(admin.ModelAdmin):
 
         url_patterns = [
             path('export/', self.admin_site.admin_view(self.get_form_export_view()), name=self.get_admin_url('export')),
+            path("webhook-export/", self.admin_site.admin_view(self.webhook_export), name="webhook_export"),
+            path("webhook-send/", self.admin_site.admin_view(self.webhook_send), name="webhook_send"),
         ]
         return url_patterns + super(BaseFormSubmissionAdmin, self).get_urls()
 
@@ -219,3 +245,76 @@ class BaseFormSubmissionAdmin(admin.ModelAdmin):
             return submission_field(obj)
         return ''
     display_data.short_description = _('data')
+
+    def get_select_webhook_form(self) -> forms.Form:
+        return type("SelectWebhookForm", (forms.Form,), {
+            "webhook": forms.ChoiceField(choices=Webhook.objects.values_list("pk", "name").order_by("name")),
+        })
+
+    def export_submissions_by_webhook(
+        self, request: HttpRequest, submissions: FormSubmission, webhook: Webhook
+    ) -> JsonResponse:
+        site = Site.objects.first()
+        data = collect_submissions_data(webhook, submissions, site.domain)
+        response = JsonResponse({"data": data}, encoder=PrettyJsonEncoder, json_dumps_params={"ensure_ascii": False})
+        filename = f"form-submissions-webhook-{slugify(webhook.name)}.json"
+        response["Content-Disposition"] = f"attachment; filename={filename}"
+        return response
+
+    def send_submissions_data(
+        self, request: HttpRequest, submissions: FormSubmission, webhook: Webhook
+    ) -> HttpResponseRedirect:
+        site = Site.objects.first()
+        send_submissions_data(webhook, submissions, site.domain)
+        messages.success(request, _("Data sending completed."))
+        return HttpResponseRedirect(reverse("admin:aldryn_forms_formsubmission_changelist"))
+
+    def process_webhook(self, request: HttpRequest, process_fnc: Callable, process_title: str) -> HttpResponse:
+        ids = request.GET.get("ids", "")
+        submissions = FormSubmission.objects.filter(pk__in=ids.split("."))
+        SelectWebhookForm = self.get_select_webhook_form()
+        if request.method == "POST":
+            if submissions.count():
+                form = SelectWebhookForm(request.POST)
+                if form.is_valid():
+                    try:
+                        webhook = Webhook.objects.get(pk=form.cleaned_data["webhook"])
+                    except Webhook.DoesNotExist as err:
+                        messages.error(request, err)
+                    else:
+                        return process_fnc(request, submissions, webhook)
+            else:
+                messages.error(request, _("Missing items for processing."))
+            return HttpResponseRedirect(reverse("admin:aldryn_forms_formsubmission_changelist"))
+        else:
+            form = SelectWebhookForm()
+        data = {
+            "ids": ids,
+            "form": form,
+            "submissins_size": submissions.count(),
+            "process_title": process_title,
+        }
+        context = dict(self.admin_site.each_context(request), **data)
+        return TemplateResponse(request, "admin/aldryn_forms/formsubmission/webhook_form.html", context)
+
+    def webhook_export(self, request: HttpRequest) -> HttpResponse:
+        return self.process_webhook(request, self.export_submissions_by_webhook, _("Export data via webhook"))
+
+    def webhook_send(self, request: HttpRequest) -> HttpResponse:
+        return self.process_webhook(request, self.send_submissions_data, _("Send data via webhook"))
+
+    def process_response_redirect(self, queryset: QuerySet, path_name: str) -> HttpResponseRedirect:
+        selected = queryset.values_list("pk", flat=True)
+        params = urlencode({"ids": ".".join([str(pk) for pk in selected])})
+        path = reverse(path_name)
+        return HttpResponseRedirect(f"{path}?{params}")
+
+    @admin.action(description=_("Export data via webhook"), permissions=['change'])
+    def export_webhook(self, request: HttpRequest, queryset: QuerySet) -> HttpResponseRedirect:
+        return self.process_response_redirect(queryset, "admin:webhook_export")
+    export_webhook.short_description = _("Export data via webhook")
+
+    @admin.action(description=_("Send data via webhook"), permissions=['change'])
+    def send_webhook(self, request: HttpRequest, queryset: QuerySet) -> HttpResponseRedirect:
+        return self.process_response_redirect(queryset, "admin:webhook_send")
+    send_webhook.short_description = _("Send data via webhook")
